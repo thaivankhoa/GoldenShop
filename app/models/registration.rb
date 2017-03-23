f5505e87f4ef21b593ad504e6da60e6f3d997954
thaivankhoa <thaivankhoa2802@gmail.com>
2017-03-23 18:08:14 +0700

    payment feature

---
+++ app/models/registration.rb
@@ -0,0 +1,10 @@
+def process_payment
+    customer = Stripe::Customer.create email: email,
+                                       card: card_token
+
+    Stripe::Charge.create customer: customer.id,
+                          amount: course.price * 100,
+                          description: course.name,
+                          currency: 'usd'
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,6 @@ Rails.application.routes.draw do
 
   resources :orders
 
+  resources :charges
+
 end
