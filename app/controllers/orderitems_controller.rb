34dec4df6a575c9ed4c8b4d953199e52190f4f39
thaivankhoa <thaivankhoa2802@gmail.com>
2017-03-23 10:03:50 +0700

    Create order

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -10,17 +10,15 @@ class OrderitemsController < ApplicationController
 	def create
 		@orderitem = OrderItem.new(order_item_params)
 		@orderitem.cart = Cart.find(session[:cart_id])
-		@orderitem.update_column(:order_id, 1)
-		
-		if @orderitem.save 
-		debugger	
+		@orderitem.order = Order.first	
+		if @orderitem.save 	
+			debugger	
 			flash[:success] = "Order Item was succesfully created"
 			redirect_to cart_path(@orderitem.cart)
 			
 		else
-			debugger
 			redirect_to root_path
-
+			debugger
 		end
 	end
 
@@ -37,7 +35,5 @@ class OrderitemsController < ApplicationController
 		def order_item_params
 			params.require(:order_item).permit(:quantity_order, :order_item_price, :order_item_title)
 		end
-
-
 
 end		
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,13 @@ class OrdersController < ApplicationController
 	end
 
 	def create
-
+		@order = Order.new(order_params)
+		if @order.save
+			flash[:success] = "Your Order was successfully created"
+			redirect_to cart_path(Cart.find(session[:cart_id]))
+		else
+			render 'new'
+		end
 	end
 
 	def show
@@ -19,6 +25,11 @@ class OrdersController < ApplicationController
 	def destroy
 
 	end
+
+		private
+			def order_params
+				params.require(:order).permit(:total_price)
+			end
 	
 
 end		
