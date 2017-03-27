41822bc6777c3a8abacd3b236d2edba9d718dce1
thaivankhoa <thaivankhoa2802@gmail.com>
2017-03-27 14:16:23 +0700

    fix bug

--- app/models/registration.rb
+++
@@ -1,9 +0,0 @@
-def process_payment
-  customer = Stripe::Customer.create email: email,
-                                     card: card_token
-
-             Stripe::Charge.create customer: customer.id,
-                                   amount: course.price * 100,
-                                   description: course.name,
-                                   currency: 'usd'
-end
